c1a41e38b59292a1937bc69c1082e4ae5b1b21b7
Vedang Joshi <vjoshi380@berkeley.edu>
2024-11-11 15:58:41 -0800

    some progress with openpose but lacking build dependencies

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cc/ee106a/fa24/class/ee106a-abr/c206a_proj/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/cc/ee106a/fa24/class/ee106a-abr/c206a_proj/devel;/opt/ros/eecsbot_ws/devel;/opt/ros/noetic'.split(';') if '/home/cc/ee106a/fa24/class/ee106a-abr/c206a_proj/devel;/opt/ros/eecsbot_ws/devel;/opt/ros/noetic' != '' else []
