38db96371d6a7a406661ca762cbd26b78f622418
Vedang Joshi <vjoshi380@berkeley.edu>
2024-11-11 13:01:23 -0800

    added lab7 and lab8 for reference files

---
+++ lab8/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa24/class/ee106a-abr/ros_workspaces/lab8/src
+# Build directory: /home/cc/ee106a/fa24/class/ee106a-abr/ros_workspaces/lab8/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("perception")
+subdirs("plannedcntrl")
